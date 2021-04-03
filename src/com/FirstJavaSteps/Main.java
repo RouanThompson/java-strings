f45f6dafad2ab078aaebc3af0e7f53786ba44ab8
rouanthompson <rouan.thompson@gmail.com>
2021-04-03 15:34:52 -0400

    Used different methods that returns a new string object

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -3,6 +3,19 @@ package com.FirstJavaSteps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String message = "Hello World" + "!!";
+        System.out.println(message);
+        // strings are used as variables often so a shorthand version was made
+        // concat or join with + operator
+
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("W"));
+
+        System.out.println(message.replace("!!", "$$"));
+        // strings literals are immutable(cannot change) in Java
+        // Any method that modify a string will always return a new string object
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
     }
 }
